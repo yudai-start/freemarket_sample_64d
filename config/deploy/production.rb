b2cdcc21af30b0e1d52be1d601b4405342e34a8b
yudai <fukuta.yudai@gmail.com>
2019-12-04 08:03:43 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+server '18.176.74.241', user: 'ec2-user', roles: %w{app db web}
 
 
 # role-based syntax
@@ -59,4 +60,3 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '18.176.74.241', user: 'ec2-user', roles: %w{app db web}
