5f5baccb48355cf17511c9e31a5763b07244891a
Keitamidehyah <56815486+Keitamidehyah@users.noreply.github.com>
2019-12-11 20:24:42 +0900

    items model&controllerの作成

---
+++ db/migrate/20191211105802_create_items.rb
@@ -0,0 +1,17 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string     :name,                  null: false, index: true
+      t.integer    :user_id,               null: false, foreign_key: true
+      t.string     :size,                  null: false
+      t.integer    :item_status,           null: false
+      t.integer    :shipping_fee_defrayer, null: false
+      t.integer    :shipping_system,       null: false
+      t.integer    :ship_from_prefecture,  null: false
+      t.integer    :ship_date,             null: false
+      t.integer    :price,                 null: false
+      t.string     :description,           null: true
+      t.timestamps
+    end
+  end
+end
