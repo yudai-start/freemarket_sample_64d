5316a3e0f4a9d4cc48441c06b6af518a2996f89f
Keitamidehyah <56815486+Keitamidehyah@users.noreply.github.com>
2019-12-12 16:49:55 +0900

    foreignkey-re-edit
    What
    外部キーの編集

--- db/migrate/20191211105802_create_items.rb
+++ db/migrate/20191211105802_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.string     :name,                  null: false, index: true
-      t.reference  :user,                  null: false, foreign_key: true
+      t.references :user,                  null: false, foreign_key: true
       t.string     :size,                  null: false
       t.integer    :item_status,           null: false
       t.integer    :shipping_fee_defrayer, null: false
