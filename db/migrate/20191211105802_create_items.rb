d25ac830371c1cbb4d5968b8336873e2a4a88542
Keitamidehyah <56815486+Keitamidehyah@users.noreply.github.com>
2019-12-12 15:54:59 +0900

    foreignkey-exchange
    What
    外部キーの記述修正
    Why
    間違えているとテーブル間での受け渡しができないため

--- db/migrate/20191211105802_create_items.rb
+++ db/migrate/20191211105802_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.string     :name,                  null: false, index: true
-      t.integer    :user_id,               null: false, foreign_key: true
+      t.integer    :user,               null: false, foreign_key: true
       t.string     :size,                  null: false
       t.integer    :item_status,           null: false
       t.integer    :shipping_fee_defrayer, null: false
