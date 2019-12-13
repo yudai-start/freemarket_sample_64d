2a435206cdf0cf75deb23d607f9bde626fb876e0
nobbbcu <nobbcu@gmail.com>
2019-12-13 16:21:02 +0900

    added facebook signup function

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,6 +4,6 @@ class Address < ApplicationRecord
   belongs_to :user
   belongs_to_active_hash :prefecture
 
-  validates :family_name, :first_name, :family_name_kana, :first_name_kana, :post_code, :prefecture_id, :city, :street_number, :user_id, presence: true
+  validates :family_name, :first_name, :family_name_kana, :first_name_kana, :post_code, :prefecture_id, :city, :street_number, presence: true
   validates :phone_number, presence: true, format: {with: /\A\d{10,11}\z/}
 end
