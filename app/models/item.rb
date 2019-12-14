ed8d0abe4872338343a42e441eab83f3cd865a8b
suemoto <s_bdash_0802@yahoo.co.jp>
2019-12-14 18:39:12 +0900

    set_upload_image
    画像をアップできるようになったっぽい感じです。

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,12 +3,12 @@ class Item < ApplicationRecord
   has_many :comments
   has_one :review
   has_many :likes
- 
+
   has_many :images
   accepts_nested_attributes_for :images
   
-  mount_uploader :image, ImageUploader
-  
   
 
+  # validates :name, :user_id,  :item_status, :shipping_fee_defrayer,  :ship_from_prefecture, :ship_date, :price, :description,  :status, presence: true
+  
 end
