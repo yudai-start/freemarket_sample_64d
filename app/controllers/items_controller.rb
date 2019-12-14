3c31b3a677a9bfd9e8ccbe661a9bbd6c22a724e0
Keitamidehyah <56815486+Keitamidehyah@users.noreply.github.com>
2019-12-14 19:57:04 +0900

    image-途中

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,7 @@
 class ItemsController < ApplicationController
 
   def index
-    # binding.pry
-    @items = Item.all
+  #  binding.pry
+    @items = Item.all.includes(:images)
   end
-
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,3 +5,4 @@ class Item < ApplicationRecord
   has_many   :likes
   has_many   :images
 end
+
