8403e4eacd9df5bce152120b938fed07a9072255
19900410ms <57243729+19900410ms@users.noreply.github.com>
2019-12-16 18:36:28 +0900

    gretel.revise-01
    パンくずリストの修正
    マイページ、プロフィール、ログアウト画面へ実装

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -6,3 +6,18 @@ crumb :items do
   link "@item.name"
   parent :root
 end
+
+crumb :mypage do
+  link "マイページ", "/posts/mypage"
+  parent :root
+end
+
+crumb :profile do
+  link "プロフィール"
+  parent :mypage
+end
+
+crumb :logout do
+  link "ログアウト"
+  parent :mypage
+end
