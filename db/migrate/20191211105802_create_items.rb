b0186ec1d04ba8e445422a49786c28bed9523534
yudai <fukuta.yudai@gmail.com>
2019-12-18 23:29:12 +0900

    modify-item-params

--- db/migrate/20191211105802_create_items.rb
+++ db/migrate/20191211105802_create_items.rb
@@ -3,15 +3,15 @@ class CreateItems < ActiveRecord::Migration[5.2]
     create_table :items do |t|
       t.string     :name,                  null: false, index: true
       t.references :user,                  null: false, foreign_key: true
-      t.string     :size                   null: true
+      t.string     :size
       t.integer    :item_status,           null: false
       t.integer    :shipping_fee_defrayer, null: false
-      t.integer    :shipping_system        null: true
+      t.integer    :shipping_system
       t.integer    :ship_from_prefecture,  null: false
       t.integer    :ship_date,             null: false
       t.integer    :price,                 null: false
       t.string     :description,           null: false
-      t.integer    :buyer_id               null: true
+      t.integer    :buyer_id
       t.integer    :status,                null: false
       t.timestamps
     end
