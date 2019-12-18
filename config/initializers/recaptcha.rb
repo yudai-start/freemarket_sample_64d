9dcae0b1a94d7c176f07da1fc76d003d8ffc4326
yudai <fukuta.yudai@gmail.com>
2019-12-18 20:16:09 +0900

    mofify-recapcha-key

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,8 @@
 Recaptcha.configure do |config|
-  site_key = Rails.application.credentials[:recaptcha][:site_key]
-  secret_key = Rails.application.credentials[:recaptcha][:secret_key]
-  # config.site_key = ENV['RECAPTCHA_PUBLIC_KEY']
-  # config.secret_key = ENV['RECAPTCHA_PRIVATE_KEY']
+  # site_key = Rails.application.credentials[:recaptcha][:site_key]
+  # secret_key = Rails.application.credentials[:recaptcha][:secret_key]
+  config.site_key = ENV['RECAPTCHA_PUBLIC_KEY']
+  config.secret_key = ENV['RECAPTCHA_PRIVATE_KEY']
+  ENV['RECAPTCHA_SITE_KEY']   = Rails.application.credentials.recaptcha[:site_key]
+  ENV['RECAPTCHA_SECRET_KEY'] = Rails.application.credentials.recaptcha[:secret_key]
 end
