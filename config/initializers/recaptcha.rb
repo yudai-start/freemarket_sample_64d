1765cbeb60c6f5a563dde397b3724183d0ed7ccc
yudai <fukuta.yudai@gmail.com>
2019-12-18 20:01:40 +0900

    modify-recapcha-key

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|
   # config.site_key = Rails.application.credentials[:recaptcha][:site_key]
   # config.secret_key = Rails.application.credentials[:recaptcha][:secret_key]
-  config.site_key : Rails.application.credentials[:recaptcha][:site_key]
-  config.secret_key : Rails.application.credentials[:recaptcha][:secret_key]
+  config.site_key: Rails.application.credentials[:recaptcha][:site_key]
+  config.secret_key: Rails.application.credentials[:recaptcha][:secret_key]
 end
