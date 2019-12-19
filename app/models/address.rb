ef61f5b49304038ad7188255f9b4b0118ee0dd52
nobbbcu <nobbcu@gmail.com>
2019-12-19 10:33:41 +0900

    completed payjp registration

--- app/models/address.rb
+++ app/models/address.rb
@@ -5,5 +5,4 @@ class Address < ApplicationRecord
   belongs_to_active_hash :prefecture
 
   validates :family_name, :first_name, :family_name_kana, :first_name_kana, :post_code, :prefecture_id, :city, :street_number, presence: true
-  validates :phone_number, presence: true, format: {with: /\A\d{10,11}\z/}
 end
