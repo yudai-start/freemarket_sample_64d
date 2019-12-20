2be9dcb3b254f46b7bc9cedb145311cada750f2b
suemoto <s_bdash_0802@yahoo.co.jp>
2019-12-20 15:12:27 +0900

    final_commit
    不要なコメントアウトを削除
    paramsのuser_idをcrrentuser_idに修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   
-  
   belongs_to :user
   has_many :comments
   has_one :review
@@ -10,12 +9,6 @@ class Item < ApplicationRecord
   accepts_nested_attributes_for :images
   belongs_to_active_hash :item_status
   validates :name, :user_id,  :item_status_id, :shipping_fee_defrayer_id,  :ship_from_prefecture, :ship_date_id, :price, :description,  :status, presence: true 
-  
-
-
-#   validates :name, :user_id, :size, :item_status, :shipping_fee_defrayer, :shipping_system, :ship_from_prefecture, :ship_date, :price, :description, :buyer_id, :status, presence: true
-
-  
 
 end
 
