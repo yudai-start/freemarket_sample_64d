bbd8fc54b60aabf29e58e5922ec4a8f4684274c9
suemoto <s_bdash_0802@yahoo.co.jp>
2019-12-24 12:56:01 +0900

    set_sold_label
    売却済みのラベルを調整
    トップページの表示順を新しいものから表示されるように修正。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   def index
 
-    @items = Item.all.includes(:images)
+    @items = Item.all.includes(:images).order("created_at DESC")
   end
 
   def new
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
   has_many :images
   accepts_nested_attributes_for :images
   belongs_to_active_hash :item_status
-  validates :name, :user_id,  :item_status_id, :shipping_fee_defrayer_id,  :ship_from_prefecture, :ship_date_id, :price, :description,  :status, presence: true 
+  validates :name, :user_id,  :item_status_id, :shipping_fee_defrayer_id,  :ship_from_prefecture, :ship_date_id, :price, :description, presence: true 
 
 end
 
