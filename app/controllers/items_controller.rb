0fd25f54142fa08119394fe75e900a6ccfdb25f8
nobbbcu <nobbcu@gmail.com>
2019-12-24 16:16:03 +0900

    refoctored controllers

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,7 @@ class ItemsController < ApplicationController
   require "payjp"
 
   def index
-    @items = Item.where.not(status:"2").includes(:images)
+    @items = Item.all.includes(:images)
   end
 
   def show
