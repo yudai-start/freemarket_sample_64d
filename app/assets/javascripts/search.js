c15ca421fb081fafb83c3fe81f2ce556871c53cf
yudai <fukuta.yudai@gmail.com>
2019-12-24 10:00:06 +0900

    add-search.js
    非同期通信用のjsファイル作成

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,17 @@
+// $(function() {
+//     $('.item_search').on('submit', function(e) {
+//       e.preventDefault();
+//       var formData = new FormData(this);
+//       var url = $(this).attr('action')
+//       $.ajax({
+//         url: url,
+//         type: "POST",
+//         data: formData,
+//         dataType: 'json',
+//         processData: false,
+//         contentType: false
+//       .done(function(items) {
+//         console.log("ok");
+//       })
+//    })
+// })
