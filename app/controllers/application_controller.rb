9ec546272b2e405e60b1a4d20d9a6fa1e5c5a7e8
yudai <fukuta.yudai@gmail.com>
2019-12-24 16:28:57 +0900

    add-ransack-to-application-controller
    ransackを全てのページで使用できるように変更。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
+  before_action :set_search
   helper_method :current_user
+  
+  def set_search
+    @q=Item.ransack(params[:q]) #gem ransakを用いての詳細検索
+    @items=@q.result
+  end
 
   private
 
