1f2a6953e19d97e672edf6d714adafd253d28311
suemoto <s_bdash_0802@yahoo.co.jp>
2019-12-25 20:41:00 +0900

    item_new_preview
    出品のプレビュー機能実装

--- app/assets/javascripts/items_new.js
+++ app/assets/javascripts/items_new.js
@@ -74,3 +74,25 @@ $(function() {
 });
 
 
+$(function(){
+  //querySelectorでfile_fieldを取得
+  // var file_field = document.querySelector('input[type=file]')
+  //fileが選択された時に発火するイベント
+    $('#item_images_attributes_0_image').change(function(){
+      //選択したfileのオブジェクトをpropで取得
+      var file = $('input[type="file"]').prop('files')[0];
+      //FileReaderのreadAsDataURLで指定したFileオブジェクトを読み込む
+      var fileReader = new FileReader();
+      //読み込みが完了すると、srcにfileのURLを格納
+      fileReader.onloadend = function() {
+        var src = fileReader.result
+        var html= ` 
+                  <img class="item-image-preview" src="${src}">
+                  `
+        //image_box__container要素の前にhtmlを差し込む
+        $(".item-image-preview").remove() 
+        $('.item-image-label').prepend(html);
+      }
+      fileReader.readAsDataURL(file);
+    });
+});
