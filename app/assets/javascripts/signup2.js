30fdc0329080eb510e560c09a1487dad140c86a1
nobbbcu <nobbcu@gmail.com>
2019-12-25 17:54:09 +0900

    added javascript inspecting registered email/phone_number

--- app/assets/javascripts/signup2.js
+++ app/assets/javascripts/signup2.js
@@ -2,13 +2,18 @@ $(function(){
   $("#phone").on('submit', function(){
     var error = 0;
     var error_format = '<span class="error">フォーマットが不適切です</span>';
-
-    let regex_phone    = new RegExp(/^[0-9]{10,11}$/);
+    var error_registered = '<span class="error">入力頂いた電話番号は登録済みです。</span>';
+    var regex_phone    = new RegExp(/^[0-9]{10,11}$/);
     var phone = $("#phone_number").val();
+    var phonelist = $("#phonelist").val();
 
     $(".error").remove()
 
     if(phone.match(regex_phone)){
+      if(phonelist.includes(phone)){
+        $(".signup2__contents__main__form1").after(error_registered);
+        error++;
+      }
     }else{
       $(".signup2__contents__main__form1").after(error_format);
       error++;
