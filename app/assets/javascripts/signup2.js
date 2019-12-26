93501c7e55a56213a57cc53fe841967af06b0b83
nobbbcu <nobbcu@gmail.com>
2019-12-26 15:06:59 +0900

    Update signup2.js

--- app/assets/javascripts/signup2.js
+++ app/assets/javascripts/signup2.js
@@ -10,7 +10,7 @@ $(function(){
     $(".error").remove()
 
     if(phone.match(regex_phone)){
-      if(phonelist.indexOf(phone)){
+      if(phonelist.indexOf(phone) >= 0){
         $(".signup2__contents__main__form1").after(error_registered);
         error++;
       }
